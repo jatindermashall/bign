e785b4afb46c53d20107b7a63ea17f7eeb5977a8
jatinder kumar <jatindermashall@gmail.com>
2020-03-16 20:59:33 +0530

    About.vue

--- client/pages/About.vue
+++ client/pages/About.vue
@@ -1,46 +1,26 @@
-
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">About</h1>
-      <h2 class="subtitle">biggmind</h2>
-    </div>
+  <div>
+    <h1>About</h1>
+    <p>This is an app that displays corny dad jokes</p>
   </div>
 </template>
 
 <script>
+export default {
+  head() {
+    return {
+      title: 'About The App',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes'
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
