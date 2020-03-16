32e7f7b47b8af8783b0765d2c860c1d1b47f70be
jatinder kumar <jatindermashall@gmail.com>
2020-03-16 20:30:22 +0530

    about

--- client/pages/About.vue
+++ client/pages/About.vue
@@ -1,25 +1,15 @@
+
 <template>
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">Biggmind fff</h1>
+      <h1 class="title">About</h1>
       <h2 class="subtitle">biggmind</h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue';
-
-export default {
-  components: {
-    Logo
-  }
-};
 </script>
 
 <style>
