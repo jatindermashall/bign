63be223185aa041b49390cef29ef4735ade42483
jatindermashall <jatindermashall@gmail.com>
2020-03-17 16:43:31 +0530

    stories

---
+++ client/pages/stories/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>{{title}}</h2>
+
+    <p>
+      {{sayHello()}}-
+      <a v-bind:href="link">Google</a>
+    </p>
+    <p v-html="htmllink"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'my list',
+      link: 'http://biggmind.com',
+      htmllink: "<a href='http://google.com'>google</a>"
+    };
+  },
+  methods: {
+    sayHello() {
+      return 'Hello';
+    }
+  },
+  head() {
+    return {
+      title: 'Welcome To Bigg mind',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for Bigg mind'
+        }
+      ]
+    };
+  }
+};
+</script>
